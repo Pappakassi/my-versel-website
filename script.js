fe2edb759112040c464a22923d68264d805a37c1
Hinrik Holmfridarson Olason <hinrikholmfridarsonolason@Hinriks-Air.c.is>
2024-06-26 09:01:03 +0000

    Add interactive questionnaire with celebration effects

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // script.js
 
 document.addEventListener("DOMContentLoaded", function() {
-    var heading = document.querySelector("h1");
-    heading.addEventListener("click", function() {
-        this.textContent = "You clicked the heading!";
+    var options = document.querySelectorAll(".option");
+    options.forEach(function(option) {
+        option.addEventListener("click", function() {
+            showCelebration();
+        });
     });
 });
+
+function showCelebration() {
+    var ribbons = document.createElement("div");
+    ribbons.className = "ribbons";
+    document.body.appendChild(ribbons);
+
+    var fireworks = document.createElement("div");
+    fireworks.className = "fireworks";
+    document.body.appendChild(fireworks);
+
+    setTimeout(function() {
+        ribbons.remove();
+        fireworks.remove();
+    }, 3000); // Remove celebration after 3 seconds
+}
